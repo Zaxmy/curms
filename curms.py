439dad32d63777888fa9c585a49fde1d8cd62230
Johan Zaxmy <johan@zaxmy.com>
2022-11-01 07:32:06 +0100

    Pause function, restart game after death

--- curms.py
+++ curms.py
@@ -3,11 +3,13 @@
 # Copyright Johan Zaxmy johan@zaxmy.com 
 # License GPLv3
 
+from locale import setlocale
 from termios import TIOCLINUX
 import time
 import curses
 import random
-from curses import wrapper
+import locale
+from curses import color_pair, wrapper
 
 def getbyte(number, i):
    return (number & (0xff << (i * 8))) >> (i * 8)
@@ -20,7 +22,8 @@ class Tile:
    BODY = 'O'
    TAIL =  'o'
    BAD_OBJECTS = [BODY,TAIL,'<','>','^','v','-','|','+']
-   GOOD_OBJECTS = {'¤':5,'@':10,'$':15}
+   GOOD_OBJECTS = {'¤':5,'@':10,'$':15,'§':20}
+   
    def __init__(self,x,y,d) -> None:
       self.x=x
       self.y=y
@@ -81,16 +84,14 @@ class Wurm():
       state = Wurm.NOTHING
       
       # Check head coord on screen
-      coll = surface.inch(self.head.y,self.head.x)
-      coll = getbyte(coll,0)
-      #surface.addstr(2,2,chr(coll))
-      #surface.addstr(3,2,"%4x"%coll,curses.color_pair(2))
+      coll = get_char(self.head.x,self.head.y,surface)
+
       # Did we hit anything bad?
-      if chr(coll) in Tile.BAD_OBJECTS:
+      if coll in Tile.BAD_OBJECTS:
          return Wurm.DIED
       # Anything we can eat?
-      if chr(coll) in Tile.GOOD_OBJECTS.keys():
-         self.add += Tile.GOOD_OBJECTS[chr(coll)]
+      if coll in Tile.GOOD_OBJECTS.keys():
+         self.add += Tile.GOOD_OBJECTS[coll]
          state=Wurm.ATE
       # Draw snake
       self.head.draw(surface)
@@ -117,31 +118,29 @@ class Wurm():
          self.head.b="<"
       elif direction == Tile.LEFT:
          self.head.b=">"
-      
-#
-
+    
 def collision(char):
    return char in Tile.BAD_OBJECTS or char in Tile.GOOD_OBJECTS.keys() 
 
 def get_char(x,y,surface):
    char = surface.inch(y,x)
    return chr(getbyte(char,0))
- 
+   
 def draw_fruit(x,y,surface):
    fruit = random.choice([f for f in Tile.GOOD_OBJECTS.keys()])
    surface.addstr(y,x,fruit,curses.color_pair(3))
  
 def add_fruit(surface):
-   nx = random.randint(2,curses.COLS-1)
-   ny = random.randint(2,curses.LINES-1)
+   nx = random.randint(1,curses.COLS-1)
+   ny = random.randint(1,curses.LINES-2)
    coll = get_char(nx,ny,surface)
    
    # Did we pick a bad space ?
    if collision(coll):
       # Mitigiation 1:st approach, test 10 more squares
       for _ in range(0,10):       
-         nx = random.randint(2,curses.COLS-1)
-         ny = random.randint(2,curses.LINES-1)
+         nx = random.randint(1,curses.COLS-1)
+         ny = random.randint(1,curses.LINES-2)
          coll = get_char(nx,ny,surface)
          if not collision(coll):
             draw_fruit(nx,ny,surface)
@@ -156,7 +155,7 @@ def add_fruit(surface):
             ny += 1
             nx = 1
          # No free space he wins!
-         if ny>curses.LINES-2:
+         if ny>curses.LINES-1:
             return False
          coll = get_char(nx,ny,surface)
 
@@ -171,9 +170,9 @@ def pause_game(surface):
    tlx = curses.COLS//2-width//2
    tly = curses.LINES//2-height//2
    win = curses.newwin(height,width,tly,tlx)
-   win.border('|','|','-','-','+','+','+','+') 
+   win.box() 
    win.addstr(1,width//2-len(msg)//2,msg,curses.color_pair(2))
-   win.addstr(2,2,msg2,curses.color_pair(2))
+   win.addstr(2,2,msg2,curses.color_pair(3))
    win.refresh()
    # getch is blocking again =)
    _ = win.getch()
@@ -181,6 +180,34 @@ def pause_game(surface):
    surface.touchwin()
    surface.refresh()
 
+def high_score(surface):
+   msg = "Highscores"
+   height = 14
+   width = len(msg)+8
+   tlx = curses.COLS//2-width//2
+   tly = curses.LINES//2-height//2
+   win = curses.newwin(height,width,tly,tlx)
+   win.box() 
+   win.addch(2,0,curses.ACS_LTEE)
+   win.hline(2,1,curses.ACS_HLINE,width-2)
+   win.addch(2,width-1,curses.ACS_RTEE)
+   win.addstr(1,width//2-len(msg)//2,msg,curses.color_pair(3))
+   win.refresh()
+   # getch is blocking again =)
+   _ = win.getch()
+   del win
+   surface.touchwin()
+   surface.refresh()
+
+def draw_main(stdscr):
+   stdscr.clear()
+   stdscr.border('|','|','-',' ','+','+',' ',' ')
+   stdscr.addstr(curses.LINES-2,0,'+')
+   stdscr.hline(curses.LINES-2,1,'-',curses.COLS-2)
+   stdscr.addstr(curses.LINES-2,curses.COLS-1,'+')
+   for i in range(5):
+      add_fruit(stdscr)
+   
 def main(stdscr):
    stdscr.clear()
    curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_GREEN)
@@ -193,14 +220,11 @@ def main(stdscr):
    # Non blocking getch() and getkey()
    stdscr.nodelay(True)
    wurm = Wurm()
-   # window.border([ls[, rs[, ts[, bs[, tl[, tr[, bl[, br]]]]]]]])
-   stdscr.border('|','|','-','-','+','+','+','+')
+   draw_main(stdscr)
    wurm.draw(stdscr)
-   # Add some test fruit
-   for i in range(5):
-      add_fruit(stdscr)
    stdscr.refresh()
    curses.curs_set(0)
+   high_score(stdscr)
    while True:
       
       c=stdscr.getch()
@@ -214,11 +238,9 @@ def main(stdscr):
          wurm.turn(Tile.LEFT)
       elif c == curses.KEY_RIGHT:
          wurm.turn(Tile.RIGHT)
-      elif c == ord('e'):
-         wurm.add += 1
       elif c == ord('p'):
          pause_game(stdscr)
- 
+      
       wurm.move()
       state = wurm.draw(stdscr) 
       if(state == Wurm.DIED):
@@ -226,7 +248,10 @@ def main(stdscr):
          stdscr.addstr(curses.LINES//2,curses.COLS//2-len(go)//2,go,curses.color_pair(2))
          stdscr.refresh()
          time.sleep(5)
-         break
+         draw_main(stdscr)
+         del wurm
+         wurm = Wurm()
+         pause_game(stdscr)
       elif (state == Wurm.ATE):
          if not add_fruit(stdscr):
             go = "-=[ YOU BEAT THE GAME ]=-"
@@ -236,4 +261,5 @@ def main(stdscr):
       stdscr.refresh()
       time.sleep(0.1)
 
+locale.setlocale(locale.LC_ALL, '')
 wrapper(main)
